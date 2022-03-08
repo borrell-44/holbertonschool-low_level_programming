ebf3580fdb8a37e2b14d9a67cd40f0dc575e4428
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-08 22:32:08 +0000

    Hot Fix

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,17 +2,14 @@
 #include <stdlib.h>
 
 /**
- * void free_dog - main function
+ * free_dog - main function
  * @d: Pointer to the struct
  *
  * Return: free pointer
  */
 
 void free_dog(dog_t *d)
-{
-	free(d->name);
-	free(d->age);
-	free(d->owner);
+{`
 	free(d);
 }
 
