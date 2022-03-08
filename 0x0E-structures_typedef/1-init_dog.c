e35cd4728f4bb1f96e3898d402803301e2498a51
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-08 02:02:28 +0000

    Commiting 1 modifying dog.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * init_dog - entry point
+ * @d: Variable to be evaluated
+ * @name: Variable to be evaluated
+ * @age: Variable to be evaluated
+ * @owner: Variable to be evaluated
+ *
+ * Return: create struct
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
+#endif
+
