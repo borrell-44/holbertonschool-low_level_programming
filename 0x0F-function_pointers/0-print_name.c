288f67b5a422a46c818eade98f182edcced7758a
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-09 16:14:17 +0000

    Commiting 0, 1, and header file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - entry point
+ * @name: Name to print
+ * @f: Pointer for printing name
+ *
+ * Return: Function result
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
+
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - entry point
+ * @array: pointer array to go through
+ * @size: size of the array
+ * @action: pointer function to print
+ *
+ * Return: executed function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+}
+
