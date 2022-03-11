053e81c2eca5e6cb6a02116603011246a60b7872
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-11 18:30:33 +0000

    Betty style on the files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -12,8 +12,7 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = 
-	{
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -21,7 +20,7 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i= 0;
+	int i = 0;
 
 	while (i < 5)
 	{
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,21 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * op_add - add numbers
+ * @a: Variable to evaluate
+ * @b: Variable to evaluate
+ *
+ * Return: sum
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - sub numbers
+ * @a: Variable to evaluate
+ * @b: Variable to evaluate
+ *
+ * Return: sub
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - mul numbers
+ * @a: Variable to evaluate
+ * @b: Variable to evaluate
+ *
+ * Return: mul
+ */
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
 
+/**
+ * op_div - div numbers
+ * @a: Variable to evaluate
+ * @b: Variable to evaluate
+ *
+ * Return: div
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -24,9 +52,16 @@ int op_div(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-        return (a / b);
+	return (a / b);
 }
 
+/**
+ * op_mod - mod numbers
+ * @a: Variable to evaluate
+ * @b: Variable to evaluate
+ *
+ * Return: mod
+ */
 int op_mod(int a, int b)
 {
 	if (b == 0)
@@ -35,6 +70,6 @@ int op_mod(int a, int b)
 		exit(100);
 	}
 
-        return (a % b);
+	return (a % b);
 }
 
