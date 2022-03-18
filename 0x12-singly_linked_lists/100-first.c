5213848f49885a15120aad39803aeca065dd9fdb
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-18 12:56:06 +0000

    Adding the betty format to the file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
+/**
+* before_main - print before the main executes
+*
+* Return: nothing
+*/
+
 void __attribute__((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
 
