fafcfecd6032d2d42a7ab6427cf0c1886297c780
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-20 20:57:59 +0000

    Adding betty format

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,7 +2,7 @@
 
 /**
 * *add_nodeint_end - adds a node at the end of the list
-* @h: the head of the list
+* @head: the head of the list
 * @n: data for the new node
 *
 * Return: address of the new element
