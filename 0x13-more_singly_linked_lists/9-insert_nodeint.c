224239dbcaa04184539092b0e3ec221802f7a2b3
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-21 14:26:48 +0000

    Hot Fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,25 +1,6 @@
 #include "lists.h"
 
 /**
-* listint_len - get the number of elements in a list
-* @h: the head of the list
-*
-* Return: number of elements
-*/
-
-size_t listint_len(const listint_t *h)
-{
-	int nodes = 0;
-
-	while (h != NULL)
-	{
-		nodes++;
-		h = h->next;
-	}
-	return (nodes);
-}
-
-/**
 * *insert_nodeint_at_index - insert a node at the specified position
 * @head: head of the node
 * @idx: position of the new node
@@ -31,7 +12,6 @@ size_t listint_len(const listint_t *h)
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	unsigned int end = listint_len(*head) + 1;
 	listint_t *tmp = *head;
 	listint_t *last = *head;
 	listint_t *new;
@@ -51,12 +31,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (*head);
 	}
 
-	if (end == idx)
-	{
-		add_nodeint_end(head, n);
-		return (*head);
-	}
-
 	for (i = 0; i <= idx; i++)
 	{
 		if (last == NULL)
@@ -72,6 +46,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (new);
 		}
 		last = tmp;
+
+		if (tmp->next == NULL)
+		{
+			new->n = n;
+			new->next = tmp->next;
+			last->next = new;
+			return (new);
+		}
 		tmp = tmp->next;
 	}
 
