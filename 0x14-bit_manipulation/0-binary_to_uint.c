172b7a44c90c5a6dee43e75156af69a84babbac9
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-25 13:10:05 +0000

    Commiting task 0 and adding a standard library to the header file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _pow - power of 2
+* @times: number of times is elevated
+*
+* Return: result
+*/
+
+int _pow(int times)
+{
+	int i;
+	int sum = 1;
+
+	for (i = 0; i < times; i++)
+	{
+		sum *= 2;
+	}
+
+	return (sum);
+}
+
+int _strlen(const char *string)
+{
+	int size = 0;
+
+	for (; string[size] != '\0'; size++)
+	{}
+
+	return (size);
+}
+
+/**
+* binary_to_uint - converts a binary number to an unsigned int
+* @b: string that represents the binary number
+*
+* Return: the converted number
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int i = _strlen(b) - 1;
+	int times = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+
+		if (b[i] == '1')
+		{
+			num += _pow(times);
+		}
+		i--;
+		times++;
+	}
+
+	return (num);
+}
+
