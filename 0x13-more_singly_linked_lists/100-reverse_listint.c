7a9b26470c01a711f0864397179325901f22bf76
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-25 02:52:00 +0000

    Making new directory and comiting old files

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+* *reverse_listint - reverses a linked list
+* @head: head of the list
+*
+* Return: first pointer to the reversed list
+*/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp;
+}
+
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,12 +31,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->n = n;
 		new->next = *head;
 		*head = new;
 		return (*head);
 	}
-
 
 	if (idx > i)
 	{
@@ -48,6 +46,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp->next = new;
 		return (new);
 	}
+
 	return (NULL);
 }
 
