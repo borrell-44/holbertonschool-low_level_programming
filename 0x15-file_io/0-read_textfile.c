4fdf1f6a53b05ea38fa9b4fbe9473ad50ca1b808
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-28 20:37:57 +0000

    Committing task 0 and including another library to the header file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+* read_textfile - reads a text file and prints it to POSIX standard output
+* @filename: Name of the file to read
+* @letters: number of letters it should read and print
+*
+* Return: the actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, i;
+	int count = 0;
+	char *buf;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY, 4);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buf = malloc(letters);
+	if (buf == NULL)
+	{
+		return (0);
+	}
+
+	read(fd, buf, letters);
+	buf[letters] = '\0';
+
+	for (i = 0; buf[i] != '\0'; i++)
+	{
+		_putchar(buf[i]);
+		count++;
+	}
+
+	close(fd);
+	return (count);
+}
+
