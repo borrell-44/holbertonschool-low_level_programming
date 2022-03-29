6739f9c787429020217bffbca519e92258d01c3b
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-29 18:56:52 +0000

    Hot Fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@
 
 int main(int ac, char **av)
 {
-	int fd_from, fd_to, letters, written, close_to, close_from;
+	int fd_from, fd_to, letters = 1, written, close_to, close_from;
 	char *buf[1024];
 
 	if (ac != 3)
@@ -24,32 +24,33 @@ int main(int ac, char **av)
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDIN_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 
-	letters = read(fd_from, buf, 1024);
-	if (letters == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-
-	/*Making a file and writting to it if it dosen't exist*/
-	fd_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC | O_APPEND, 0664);
+	fd_to = open(av[2], O_RDWR | O_TRUNC | O_CREATE, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDIN_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 
-	written = write(fd_to, buf, letters);
-	if (written == -1)
+	while (letters > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		letters = read(fd_from, buf, 1024);
+		if (letters == -1)
+		{
+			dprintf(STDIN_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
 
+		written = write(fd_to, buf, letters);
+		if (written == -1)
+		{
+			dprintf(STDIN_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
 
 	/*Closing files*/
 	close_from = close(fd_from);
