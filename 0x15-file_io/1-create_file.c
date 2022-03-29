7d22c98319a331082f5b88e119d3dd6812614c58
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-29 02:17:45 +0000

    Hot Fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_EXCL, 0600);
+	fd = open(filename, O_EXCL | O_CREAT | O_RDWR, 0600);
 
 	if (fd == -1)
 	{
@@ -31,6 +31,7 @@ int create_file(const char *filename, char *text_content)
 		{
 			write(fd, &text_content[i], 1);
 		}
+		return (1);
 	}
 
 	return (1);
