02869a212320bf615c83de608a07f2cf320635ad
borrell-44 <alexanderborrell44@hotmail.com>
2022-03-29 15:20:05 +0000

    Hot Fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,10 +26,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd_w = write(fd, text_content, strlen(text_content));
-	if (fd_w == -1)
+	if (text_content != NULL)
 	{
-		return (-1);
+		fd_w = write(fd, text_content, strlen(text_content));
+		if (fd_w == -1)
+		{
+			return (-1);
+		}
 	}
 
 	close(fd);
