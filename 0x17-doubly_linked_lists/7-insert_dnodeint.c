ba03423341e1fe8f380ecd6e834efc946bbdadbc
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-06 14:41:56 +0000

    Committing task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new->n = n;
-	if ((*h) == NULL || idx == i)
+	if ((*h) == NULL && idx == i)
 	{
 		new->next = (*h);
 		new->prev = NULL;
