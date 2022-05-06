9e799c2e5f9ca5e21e76d669b5be6c54448db088
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-06 15:56:04 +0000

    Adding the betty format to file

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,12 +1,19 @@
 #include "lists.h"
 
+/**
+* delete_dnodeint_at_index - deletes the specified node
+* @head: head of the linked list
+* @index: where the node to be deleted will be located
+*
+* Return: 1 if succeeded or -1 if it failed
+*/
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 	dlistint_t *tmp = *head;
 
 	if (head == NULL || tmp == NULL)
-
 	{
 		return (-1);
 	}
@@ -35,18 +42,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (i == index && tmp != NULL)
 	{
 		if (tmp->next != NULL)
-		{
 			tmp->next->prev = tmp->prev;
-		}
 
 		if (tmp->prev != NULL)
-		{
 			tmp->prev->next = tmp->next;
-		}
 
 		return (1);
 	}
-
 	return (-1);
 }
 
