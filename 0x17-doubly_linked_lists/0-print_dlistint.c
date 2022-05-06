e24366e0c6b14a06c7303f6687283b47572938da
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-06 14:32:24 +0000

    Hot Fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,16 +10,17 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i;
+	dlistint_t *tmp = h;
 
 	if (h == NULL)
 	{
 		return (0);
 	}
 
-	for (i = 0; h != NULL; i++)
+	for (i = 0; tmp != NULL; i++)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
 	}
 
 	return (i);
