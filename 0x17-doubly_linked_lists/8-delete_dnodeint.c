8d3ad5f0a16089267bfb8c9dba4f96da40e22cc8
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-06 15:28:24 +0000

    Committing task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *tmp = *head;
+
+	if (head == NULL || tmp == NULL)
+
+	{
+		return (-1);
+	}
+
+	if (index == i)
+	{
+		if ((*head)->next == NULL)
+		{
+			(*head) = NULL;
+		}
+
+		else
+		{
+			(*head) = tmp->next;
+			(*head)->prev = NULL;
+		}
+
+		return (1);
+	}
+
+	for (; i < index && tmp->next != NULL; i++)
+	{
+		tmp = tmp->next;
+	}
+
+	if (i == index && tmp != NULL)
+	{
+		if (tmp->next != NULL)
+		{
+			tmp->next->prev = tmp->prev;
+		}
+
+		if (tmp->prev != NULL)
+		{
+			tmp->prev->next = tmp->next;
+		}
+
+		return (1);
+	}
+
+	return (-1);
+}
+
