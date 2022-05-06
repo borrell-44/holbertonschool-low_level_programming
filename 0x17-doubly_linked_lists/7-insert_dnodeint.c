3d74233d7668e97ebabb9f0da87e1552efddf050
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-06 03:17:00 +0000

    Hot Fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *tmp;
 
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (new == NULL || *h == NULL)
 	{
 		return (NULL);
 	}
