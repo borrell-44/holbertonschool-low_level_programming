6aaad27d429ca84e6f376a43f51accb3c95243db
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-14 17:28:12 +0000

    Committing task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """ returns the perimeter of the island described in grid """
+
+    leg=0
+    wid=0
+    con=True
+
+    for i in range(len(grid)):
+        if 1 in grid[i]:
+            leg += 1
+        for j in range(len(grid[i])):
+            con=False
+            if grid[i][j] == 1:
+                if j != 0:
+                    if grid[i][j - 1] == 1:
+                        wid += 1
+                        con=True
+                if j != len(grid[i]) and con == False:
+                    if grid[i][j + 1] == 1:
+                        wid += 1
+
+    if leg == 0 and wid >= 1:
+        leg += 1
+    if wid == 0 and leg >= 1:
+        wid += 1
+
+    print("leg: {}\nwid: {}".format(leg, wid))
+
+    return (leg + wid) * 2
