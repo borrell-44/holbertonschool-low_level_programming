e548420714700b62b1fb25e64451660b7a850bbe
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-14 17:33:16 +0000

    Adding pycodetyle to file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,21 +3,21 @@
 def island_perimeter(grid):
     """ returns the perimeter of the island described in grid """
 
-    leg=0
-    wid=0
-    con=True
+    leg = 0
+    wid = 0
+    con = True
 
     for i in range(len(grid)):
         if 1 in grid[i]:
             leg += 1
         for j in range(len(grid[i])):
-            con=False
+            con = False
             if grid[i][j] == 1:
                 if j != 0:
                     if grid[i][j - 1] == 1:
                         wid += 1
-                        con=True
-                if j != len(grid[i]) and con == False:
+                        con = True
+                if j != len(grid[i]) and con is False:
                     if grid[i][j + 1] == 1:
                         wid += 1
 
