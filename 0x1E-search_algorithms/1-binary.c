9657feb67817701b6e7767a3d6ef206c62a59ab3
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-29 12:11:18 -0400

    Committing task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+* binary_search - Searches for a value in a list using BInary search
+* @array: pointer to the first element of the array to search in
+* @size: number of elements in the array
+* @value: value to search for
+*
+* Return: index where value was faund or -1 if not
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, mid, start = 0, end = size - 1;
+
+    while(start <= end)
+    {
+        printf("Searching in array:");
+        for(i = start; i <= end; i++)
+        {
+            printf(" %d", array[i]);
+            if(i != end)
+            {
+                printf(",");
+            }
+        }
+        printf("\n");
+
+        mid = start + (end - start) / 2;
+        if(array[mid] == value)
+        {
+            return (mid);
+        }
+        if(array[mid] < value)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+	return (-1);
+}
