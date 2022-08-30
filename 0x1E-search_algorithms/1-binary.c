0fe3ab9bd667b9e689791360c637f5e5c741d0d1
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-30 13:16:10 -0400

    Hot Fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,6 +13,11 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t i, mid, start = 0, end = size - 1;
 
+    if (array == NULL)
+    {
+        return (-1);
+    }
+
     while(start <= end)
     {
         printf("Searching in array:");
