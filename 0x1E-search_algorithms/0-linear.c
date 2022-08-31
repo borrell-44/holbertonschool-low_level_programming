3a1119c04a9e10963ab81ae8a72e1187624b7395
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-31 13:04:54 -0400

    Hot Fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
 * linear_search - Searches for a value in a list using Linear search
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,37 +13,37 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t i, mid, start = 0, end = size - 1;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-    while(start <= end)
-    {
-        printf("Searching in array:");
-        for(i = start; i <= end; i++)
-        {
-            printf(" %d", array[i]);
-            if(i != end)
-            {
-                printf(",");
-            }
-        }
-        printf("\n");
+	while(start <= end)
+	{
+		printf("Searching in array:");
+		for(i = start; i <= end; i++)
+		{
+			printf(" %d", array[i]);
+			if(i != end)
+			{
+				printf(",");
+			}
+		}
+		printf("\n");
 
-        mid = start + (end - start) / 2;
-        if(array[mid] == value)
-        {
-            return (mid);
-        }
-        if(array[mid] < value)
-        {
-            start = mid + 1;
-        }
-        else
-        {
-            end = mid - 1;
-        }
-    }
+		mid = start + (end - start) / 2;
+		if(array[mid] == value)
+		{
+			return (mid);
+		}
+		if(array[mid] < value)
+		{
+			start = mid + 1;
+		}
+		else
+		{
+			end = mid - 1;
+		}
+	}
 	return (-1);
 }
